12cef52a8dcb3dd18d38cb7e7e5d254c9d758eee
98kej <56228483+98kej@users.noreply.github.com>
2020-02-23 02:52:22 +0900

    homepage1 진행중

--- src/components/Home1.jsx
+++ src/components/Home1.jsx
@@ -1,32 +1,27 @@
 import React from 'react';
 import Logo from '../components/Logo';
+import Home_img1 from '../image/home/home1/자산 1.png'
 
 const Home1= () => {
 
     return(
-        <div>
+        <div className="home_1">
             <Logo/>
 
            {/*메인화면 이미지  png*/}
-            <img src="../image/home/home1/자산 1.png"
+            <img src={Home_img1}
      srcset="../image/home/home1/자산 1@2x.png 2x,
              ../image/home1/자산 1@3x.png 3x"
 
              alt="메인 화면 이미지"
      className="home_img"/> 
 
-           {/* jpg */} 
-      <img src="../image/home/home1/자산 1.jpg"
-     srcset="../image/home/home1/자산 1@2x.jpg 2x,
-             ../image/home1/자산 1@3x.jpg 3x"
-
-             alt="메인화면 이미지"
-     className="home_img"/>
-
+        
          {/* 검색창 */}
-
+        
          <input placeholder="정보 나누고 정보 얻고!" className="home_search"></input>
-         <button>검색</button>
+         <button></button>
+         
 
         </div>
 
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo1 from '../image/home/logo/그룹 69.png'
 
 
 
@@ -7,22 +8,17 @@ const Logo=() => {
     return(
 
         <div>
-            
-           <img src="../image/home/logo/그룹 69.png"
+            {/* png */}
+           <img src={Logo1}
                 srcset="../image/home/logo/그룹 69@2x.png 2x,
                      ../image/home/logo/그룹 69@3x.png 3x"
 
                      alt="홈페이지 로고"
 
                 className="home_logo"/> 
-
-                 
-          <img src="../image/home/logo/그룹 69.jpg"
-               srcset="../image/그룹 69@2x.jpg 2x,
-                       ../image/그룹 69@3x.jpg 3x"
-
-                       alt="홈페이지 로고"
-               className="home_logo"/>
+                
+              
+             
         </div>
 
     );
