876b24e01fe674e3e32ec24450de998a194b24e0
98kej <56228483+98kej@users.noreply.github.com>
2020-07-08 21:36:01 +0900

    Home_2 진행중

--- src/components/Home2.jsx
+++ src/components/Home2.jsx
@@ -7,8 +7,8 @@ const Home2 = () => {
     <div className="home_2">
       {/*빠른메뉴 */}
       <div className="menu_Q">
-        <p>빠른메뉴</p>
-        <button className="Q_btn" onClick="">
+        <p className="bold_p">빠른메뉴</p>
+        <button type="button" className="Q_btn" onClick="">
           <img
             src={Img_1}
             srcset="../image/home/quickmenu/btn_자료올리기@2x.png 2x,
@@ -16,7 +16,7 @@ const Home2 = () => {
             alt="자료올리기 버튼"
           />
         </button>
-        <button className="Q_btn">
+        <button type="button" className="Q_btn">
           <img
             src={Img_2}
             srcset="../image/home/quickmenu/btn_학과별자료@2x.png 2x,
@@ -24,7 +24,7 @@ const Home2 = () => {
             alt="학과별자료 버튼"
           />
         </button>
-        <button className="Q_btn">
+        <button type="button" className="Q_btn">
           <img
             src={Img_3}
             srcset="../image/home/quickmenu/btn_교양자료@2x.png 2x,
@@ -36,15 +36,79 @@ const Home2 = () => {
 
       {/*공지사항 */}
       <div className="Notice">
-        <a href="">공지사항 +</a>
+        <div className="">
+          <p className="bold_p">공지사항</p>
+          <a className="plus"></a>
+        </div>
+        <hr></hr>
+        <a>
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <hr></hr>
       </div>
       {/*우수자료 */}
       <div className="good_data">
-        <p>우수자료</p>
+        <p className="bold_p">우수자료</p>
+        <hr></hr>
+        <a>
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <a>
+          {" "}
+          <br></br>
+        </a>
+        <hr></hr>
       </div>
       {/*로그인*/}
       <div className="login">
-        <p>로그인</p>
+        <p className="bold_p">로그인</p>
+        <form method="post" action="">
+          <input className="login_input" type="text" placeholder="아이디"></input>
+          <input className="login_input" type="password" placeholder="비밀번호"></input>
+          <a href="" className="id_pw">
+            아이디/비밀번호가 기억나지 않으세요?
+          </a>
+          <div className="flex">
+            <button></button>
+            <input></input>
+          </div>
+        </form>
       </div>
     </div>
   );
